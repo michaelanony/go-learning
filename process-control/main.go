4663f7d9957dcd6ce24fa0aac8bf970dee021f10
bchen5 <bchen5@ebay.com>
2019-11-22 14:53:32 +0800

    11.22

---
+++ process-control/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 19
+	if age>18{
+		fmt.Println("over 18")
+	}else if age<18{
+		fmt.Println("under 18")
+	}else{
+		fmt.Println("equal 18")
+	}
+
+	if age2:=28;age2>18{
+		fmt.Println("over 18")
+	}
+	for i:=0;i<10;i++{
+		fmt.Println(i)
+	}
+	for age>0{
+		fmt.Println(age)
+		age = age-1
+	}
+	switch age {
+	case 1:
+		fmt.Println("1")
+	default:
+		fmt.Println("default")
+	}
+}
