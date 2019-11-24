13ee681ba8a9304d8c9a8c8e7e3d8638bedb982c
“michael <354307169@qq.com>
2019-11-24 16:37:23 +0800

    11.24

--- oop/interface/example3(extend)/main.go
+++ oop/interface/example3(extend)/main.go
@@ -5,20 +5,27 @@ import "fmt"
 type Monkey struct{
 	Name string
 }
-
-func (mk *Monkey) climbing{
+//声明接口
+type BirdAble interface {
+	Flying()
+}
+func (mk *Monkey) climbing(){
 	fmt.Println(mk.Name,"Climbing")
 }
 
 //LittleMonkey
 type LittleMonkey struct{
 	Monkey
-
+}
+func (mk *LittleMonkey) Flying(){
+	fmt.Println(mk.Name,"Flying")
 }
 func main() {
-	mk = LittleMonkey{
+	mk := LittleMonkey{
 		Monkey{
 			Name:"WuKong",
 		},
 	}
+	mk.climbing()
+	mk.Flying()
 }
