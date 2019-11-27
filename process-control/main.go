82880292830b1d3fb5f1dc6f59fa8282b469a535
bchen5 <bchen5@ebay.com>
2019-11-27 17:04:58 +0800

    11.26

--- process-control/main.go
+++ process-control/main.go
@@ -25,7 +25,29 @@ func main() {
 	switch age {
 	case 1:
 		fmt.Println("1")
+		fallthrough //无条件执行下面的case语句
+	case 2,4,5:
+		fmt.Println("245")
 	default:
 		fmt.Println("default")
+	}
+	switch  {
+	case age>2:
+		fmt.Println(">2")
+	case age==2:
+		fmt.Println("2")
+	default:
+		fmt.Println("<2")
+	}
+
+	for i:=0;i<5;i++{
+		for j:=0;j<3;j++{
+			if i==2 && j==2{
+				//break//跳出for循环
+				continue
+			}
+			fmt.Println(i,j)
+
+		}
 	}
 }
