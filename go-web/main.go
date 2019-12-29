0a7658a1f26a50acefbe9b9af7f1157e30a1d7b4
“michael <354307169@qq.com>
2019-12-29 16:30:07 +0800

    add goweb

---
+++ go-web/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func indexView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello"))
+}
+
+func listView(w http.ResponseWriter, r *http.Request) {
+	buf, _ := ioutil.ReadFile("templates/list.html")
+	w.Write(buf)
+}
+func userAll(w http.ResponseWriter, r *http.Request) {
+	mods, _ := UserAll()
+	buf, _ := json.Marshal(mods)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(buf)
+}
+func userOne(w http.ResponseWriter, r *http.Request) {
+	mods, _ := UseOne()
+	buf, _ := json.Marshal(mods)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(buf)
+}
+func update(w http.ResponseWriter, r *http.Request) {
+	mods, _ := Update()
+	buf, _ := json.Marshal(mods)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(buf)
+}
+
+func lyStaticNum(w http.ResponseWriter, r *http.Request) {
+	mods, _ := LyStaticNum()
+	buf, _ := json.Marshal(mods)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(buf)
+}
+func main() {
+	http.HandleFunc(`/`, listView)
+	http.HandleFunc(`/user`, userAll)
+	http.HandleFunc(`/user1`, userOne)
+	http.HandleFunc(`/userupdate`, update)
+	http.HandleFunc(`/ly`, lyStaticNum)
+	http.ListenAndServe(":8081", nil)
+	fmt.Println("run")
+}
