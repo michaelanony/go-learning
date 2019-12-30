bcb62a65dbb12d84552ac00a7ed0c879d1a035e4
Michael Chen <michael@MichaeldeMBP.lan>
2019-12-30 23:50:26 +0800

    del exec

--- src/grpc-learning/doubleParm/client.go
+++ src/grpc-learning/doubleParm/client.go
@@ -10,20 +10,15 @@ func main() {
 	if err!=nil{
 		panic(err.Error())
 	}
-	var req float32 //请求值
-	req=3
+
 	var resp *float32//返回值
 
+	param := &AddParma{Args1:1.2,Args2:2.3}
 	//同步调用
-	//err = client.Call("MathUtil.Cal",req,&resp)
-	//if err!=nil{
-	//	panic(err.Error())
-	//}
-	//fmt.Println(*resp)
-
-	//异步调用
-	syncCall := client.Go("MathUtil.Cal",req,&resp,nil)
-	replayDone := <-syncCall.Done
-	fmt.Println(replayDone)
+	err = client.Call("MathUtil.CalTwo",param,&resp)
+	if err!=nil{
+		panic(err.Error())
+	}
 	fmt.Println(*resp)
+
 }
--- src/grpc-learning/doubleParm/server.go
+++ src/grpc-learning/doubleParm/server.go
@@ -17,14 +17,14 @@ func main() {
 	//1、初始化指针类型
 	ma := new(MathUtil)
 	//2、注册服务对象
-	err:=rpc.Register(ma)
+	err:=rpc.RegisterName("MathUtil",ma)//自定义服务名
 	if err!=nil{
 		panic(err.Error())
 	}
 	//3、将mathutil提供的服务注册到http上
 	rpc.HandleHTTP()
 	//4、在特定端口监听
-	listen,err:=net.Listen("tcp","127.0.0.1:8081")
+	listen,err:=net.Listen("tcp","127.0.0.1:8082")
 	if err!=nil{
 		panic(err.Error())
 	}
