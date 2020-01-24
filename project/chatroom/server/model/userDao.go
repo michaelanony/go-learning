f96bad03965742042de6667edf97fec935bfeb19
michaelanony <354307169@qq.com>
2020-01-24 12:35:32 +0800

    merge

--- project/chatroom/server/model/userDao.go
+++ project/chatroom/server/model/userDao.go
@@ -6,71 +6,75 @@ import (
 	"github.com/garyburd/redigo/redis"
 	"go-learning/project/chatroom/common/message"
 )
-var(
+
+var (
 	MyUserDao *UserDao
 )
+
 //定义一个UserDao结构体，完成对User结构体的各种操作
 type UserDao struct {
 	pool *redis.Pool
-
 }
+
 //使用工厂函数，创建一个UserDao实例
-func NewUserDao(pool *redis.Pool)  (userDao *UserDao){
-	userDao =&UserDao{
-		pool:pool,
+func NewUserDao(pool *redis.Pool) (userDao *UserDao) {
+	userDao = &UserDao{
+		pool: pool,
 	}
 	return
 }
+
 //1、根据用户id返回一个User实例+err
-func (this *UserDao) getUserById(conn redis.Conn,id int) (user *User ,err error){
+func (this *UserDao) getUserById(conn redis.Conn, id int) (user *User, err error) {
 	//通过给定的ID去redis查询这个用户
-	res,err:=redis.String(conn.Do("HGET","users",id))
-	if err!=nil{
-		if err ==redis.ErrNil{
-			err =ERROR_USER_NOT_EXISTS
+	res, err := redis.String(conn.Do("HGET", "users", id))
+	if err != nil {
+		if err == redis.ErrNil {
+			err = ERROR_USER_NOT_EXISTS
 		}
 		return
 	}
 	user = &User{}
-	err = json.Unmarshal([]byte(res),user)
-	if err!=nil{
+	err = json.Unmarshal([]byte(res), user)
+	if err != nil {
 		fmt.Println("反序列化出错")
 		return
 	}
-	return 
+	return
 }
 
 //完成登入的校验
-func (this *UserDao) Login(userId int,userPwd string)(user *User,err error)  {
-	conn :=this.pool.Get()
+func (this *UserDao) Login(userId int, userPwd string) (user *User, err error) {
+	conn := this.pool.Get()
 	defer conn.Close()
-	user,err =this.getUserById(conn,userId)
-	if err!=nil{
+	user, err = this.getUserById(conn, userId)
+	if err != nil {
 		return
 	}
-	if user.UserPwd != userPwd{
+	if user.UserPwd != userPwd {
 		err = ERROR_USER_PWD
 		return
 	}
 	return
 }
 
-
-func (this *UserDao) Register(user *message.User)(err error)  {
-	conn :=this.pool.Get()
+func (this *UserDao) Register(user *message.User) (err error) {
+	//先从UserDao连接池取出一根连接
+	conn := this.pool.Get()
 	defer conn.Close()
-	_,err =this.getUserById(conn,user.UserId)
-	if err==nil{
+	_, err = this.getUserById(conn, user.UserId)
+	if err == nil {
 		err = ERROR_USER_EXISTS
 		return
 	}
-	data,err := json.Marshal(user)
-	if err!=nil{
+	data, err := json.Marshal(user)
+	if err != nil {
 		return
 	}
-	_,err =conn.Do("HSet","users",user.UserId,string(data))
-	if err!=nil{
-		fmt.Println("保存注册用户错误 err=",err)
+	//入库
+	_, err = conn.Do("HSet", "users", user.UserId, string(data))
+	if err != nil {
+		fmt.Println("保存注册用户错误 err=", err)
 		return
 	}
 	return
